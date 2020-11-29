300fe25f0d937f74ebc6f51ed21bfdc10138af32
Sankalpa Senevirathne <dev.sanklpa97@gmail.com>
2020-11-29 11:37:17 +0530

    init commit

---
+++ CL-BSCSD-20-12/MyProject/src/main/java/newpackage/Student.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package newpackage;
+
+import org.icbt.myproject.*;
+
+/**
+ *
+ * @author Sankalpa
+ */
+public class Student extends Person {
+    private String id;
+
+    public Student() {
+        this.id = "";
+    }
+
+    public Student(String id, String firstName, String lastName) {
+        super(firstName, lastName);
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+    
+    @Override
+    public String getFullName() {
+        return "-----" + getFirstName() + " " + getLastName();
+    } 
+}
