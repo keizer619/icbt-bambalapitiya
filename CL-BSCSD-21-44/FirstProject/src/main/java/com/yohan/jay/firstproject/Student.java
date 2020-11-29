2df85916552e675c4be7c524d6ae5aac1d6286b9
Yohan Jayasinghe <yohanjayasinghe187@gmail.com>
2020-11-29 10:33:37 +0530

    Add project file

---
+++ CL-BSCSD-21-44/FirstProject/src/main/java/com/yohan/jay/firstproject/Student.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.yohan.jay.firstproject;
+
+/**
+ *
+ * @author YohanJayasinghe
+ */
+public class Student {
+    
+    private String firstName;
+    private String lastName;
+    
+    public String getFirstName(){
+        return this.firstName;
+    }
+    
+    public void setFirstName(String firstName){
+        this.firstName = firstName;
+    }
+    
+    public String getLastName(){
+        return this.lastName;
+    }
+    
+    public void setLastName(String lastName){
+        this.lastName = lastName;
+    }
+    
+    //public static final String STUDENT_CODE = "ST";
+    
+    //public String firstName;
+    //public String lastName;
+}
