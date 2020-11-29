95a8fc2934c7f707e789b64a52d9e63f579c12ef
Enok12 <enokdilshan115@gmail.com>
2020-11-29 10:44:37 +0530

    Java File

---
+++ CL-BSCSD-21-15/mavenproject1/src/main/java/com/mycompany/mavenproject1/Employee.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author R E V O
+ */
+public class Employee {
+    private String efNO;
+
+    public Employee(String efNO) {
+        this.efNO = efNO;
+    }
+
+    public String getEfNO() {
+        return efNO;
+    }
+
+    public void setEfNO(String efNO) {
+        this.efNO = efNO;
+    }
+    
+    
+}
---
+++ CL-BSCSD-21-15/mavenproject1/src/main/java/com/mycompany/mavenproject1/NewClass.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author R E V O
+ */
+public class NewClass {
+
+    public NewClass() {
+    }
+    
+    
+    public static void main (String [] args){
+      System.out.println("Hel World");
+  }
+}
