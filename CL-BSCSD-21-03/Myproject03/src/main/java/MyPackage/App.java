9055cd393d5ad1395f4dbc63b378b766f203acd8
FIRST_NAME LAST_NAME <68815802+Kalanirasa@users.noreply.github.com>
2020-11-29 11:13:16 +0530

    New Project

---
+++ CL-BSCSD-21-03/Myproject03/src/main/java/MyPackage/App.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MyPackage;
+
+/**
+ *
+ * @author Kalani
+ */
+public class App {
+    public static void main(String[] args) {
+        
+        
+        
+        Student st = new Student("CLBSCD2103", "Kalani", "Rasanjali");
+        System.out.println(st.getFullName());
+    }
+}
