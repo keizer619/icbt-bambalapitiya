fda9fdcf0dac3a65e3dc8910aa4ef28f8efedd37
htcuserwkp <73767957+htcuserwkp@users.noreply.github.com>
2020-11-29 10:29:54 +0530

    Add mavenproject1

---
+++ CL-BSCSD-21-02/mavenproject1/src/main/java/com/mycompany/mavenproject1/App.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.mavenproject1;
+
+
+public class App 
+{ 
+
+    public App() {
+    }
+    
+    public static void main(String args[])
+    {
+        System.out.println("Helo World");
+        //initializing 1
+        int x[]=new int[3];
+     
+        x[0]=10;
+        x[1]=20;
+        x[2]=30;
+        
+        
+        for(int y=0;y<x.length;y++)
+        {
+            System.out.println(x[y]);
+        }
+        
+        //initializing 2
+        int w[]={100,200,300,400,500,600};
+        for(int z=0;z<w.length;z++)
+        {
+            System.out.println(w[z]);
+        }
+    }   
+    
+}
