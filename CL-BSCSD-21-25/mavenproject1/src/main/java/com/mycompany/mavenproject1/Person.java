95a14d274f4a4eae79536727cceea21814cf93a1
Shavindya <shavindyatharushi@gmail.com>
2020-12-05 11:46:27 +0530

    updated

---
+++ CL-BSCSD-21-25/mavenproject1/src/main/java/com/mycompany/mavenproject1/Person.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author Asus
+ */
+public class Person {
+  private String firstName;
+    private String lastName;
+
+    public Person() {
+        this.firstName = "";
+        this.lastName = "";
+    }
+
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getFirstName(String a) {        
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFullName() {
+        return this.firstName + " " + this.lastName;
+    }  
+}
+  
+
