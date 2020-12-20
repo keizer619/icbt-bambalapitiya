4e9b42f202cc38aaaf953cb6940a3487a4066c2b
Tharik Kanaka <tharik.kanaka@gmail.com>
2020-12-20 11:42:35 +0530

    Include web project

--- tharik/myproject/src/main/java/process/Process.java
+++ tharik/myproject/src/main/java/process/Process.java
@@ -13,15 +13,12 @@ import db.DBUtil;
  */
 public class Process {
     public void process(DBUtil util) {
-     
         util.addStudents();
-        
         
         util.addStudents();
         
         util.updateStudents();
         
         util.deleteStudents();
-        
     }
 }
